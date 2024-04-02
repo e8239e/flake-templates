1adbfcea3e7211839f86edfe4250740c16303cac
e8239e <e8239e@gmail.com>
2024-04-02 14:43:18 +0300

    change in description generation

--- flake.nix
+++ flake.nix
@@ -3,7 +3,19 @@
 
   outputs = { ... }: rec {
     defaultTemplate = templates.generic;
-    templates = builtins.mapAttrs (k: v: { path = ./templates + "/${k}"; description = "${k}"; })
+    templates = builtins.mapAttrs
+      (k: v:
+        let
+          descriptionFromPath = path:
+            let
+              words = builtins.filter (builtins.isString) (builtins.split "_" path);
+            in
+            builtins.concatStringsSep " " words;
+        in
+        {
+          path = ./templates + "/${k}";
+          description = descriptionFromPath k;
+        })
       (builtins.readDir ./templates);
   };
 }
